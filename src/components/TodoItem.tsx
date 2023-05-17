bb8be71dfd4026cd18d392af6ac20b1dd3e5bb5c
Onllsan <welcome3me@gmail.com>
2023-05-17 20:09:15 +0200

    back

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,86 +1,27 @@
-// import React, { useState, useEffect } from "react";
-// import { Todo } from "./TodoList";
-
-// interface TodoItemProps {
-//   todo: Todo;
-// }
-
-// const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
-//   const initialState = JSON.parse(localStorage.getItem(todo.id) || "false");
-//   const [completed, setCompleted] = useState(initialState);
-
-//   useEffect(() => {
-//     localStorage.setItem(todo.id, JSON.stringify(completed));
-//   }, [completed, todo.id]);
-
-//   const handleClick = () => {
-//     setCompleted(!completed);
-//   };
-
-//   return (
-//     <div
-//       className="flex items-center p-4 gap-4  "
-//       style={{ cursor: "pointer" }}
-//     >
-//       <button
-//         className={`h-6 w-6 rounded-full border-2 flex-shrink-0 ${
-//           completed ? "border-[#D1A1B5] bg-[#E8B7CB]" : "border-gray-300"
-//         }`}
-//         onClick={handleClick}
-//       />
-//       <div
-//         className={`ml-4 text-md ${completed ? "opacity-50" : ""}`}
-//         onClick={handleClick}
-//       >
-//         {todo.description}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TodoItem;
 import React, { useState, useEffect } from "react";
 import { Todo } from "./TodoList";
-import { useMutation, gql } from "@apollo/client";
-
-const UPDATE_TODO = gql`
-  mutation UpdateTodo($id: String!, $completed: Boolean!) {
-    updateTodo(id: $id, completed: $completed) {
-      id
-      completed
-    }
-  }
-`;
 
 interface TodoItemProps {
   todo: Todo;
-  refetch: () => void;
 }
 
-const TodoItem: React.FC<TodoItemProps> = ({ todo, refetch }) => {
-  const localStorageKey = `todo-${todo.id}-completed`;
-  const completedFromLocalStorage =
-    localStorage.getItem(localStorageKey) === "true";
-  const [completed, setCompleted] = useState(completedFromLocalStorage);
-  const [updateTodo, { loading, error }] = useMutation(UPDATE_TODO);
+const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
+  const initialState = JSON.parse(localStorage.getItem(todo.id) || "false");
+  const [completed, setCompleted] = useState(initialState);
 
   useEffect(() => {
-    localStorage.setItem(localStorageKey, String(completed));
-  }, [completed, localStorageKey]);
+    localStorage.setItem(todo.id, JSON.stringify(completed));
+  }, [completed, todo.id]);
 
-  const handleClick = async () => {
+  const handleClick = () => {
     setCompleted(!completed);
-    await updateTodo({
-      variables: { id: todo.id, completed: !completed },
-    });
-    refetch();
   };
 
-  if (loading) return <p>Updating...</p>;
-  if (error) return <p>Error :(</p>;
-
   return (
-    <div className="flex items-center p-4 gap-4" style={{ cursor: "pointer" }}>
+    <div
+      className="flex items-center p-4 gap-4  "
+      style={{ cursor: "pointer" }}
+    >
       <button
         className={`h-6 w-6 rounded-full border-2 flex-shrink-0 ${
           completed ? "border-[#D1A1B5] bg-[#E8B7CB]" : "border-gray-300"
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,52 +1,3 @@
-// import React from "react";
-// import { useQuery, gql } from "@apollo/client";
-// import TodoItem from "./TodoItem";
-// import Heading from "./Heading";
-
-// const GET_TODOS = gql`
-//   query GetTodos {
-//     todos {
-//       id
-//       description
-//     }
-//   }
-// `;
-
-// export interface Todo {
-//   id: string;
-//   description: string;
-//   completed: boolean;
-// }
-
-// const TodoList: React.FC = () => {
-//   const { loading, error, data } = useQuery(GET_TODOS);
-//   console.log(data);
-
-//   if (loading) return <p>Loading...</p>;
-//   if (error) return <p>Error :</p>;
-
-//   return (
-//     <>
-//       <Heading />
-//       <div className="flex items-center justify-center flex-col m-9 ">
-//         <div className="container">
-//           <div>
-//             {data.todos.map((todo: Todo) => (
-//               <div
-//                 key={todo.id}
-//                 className="flex items-center justify-between mb-6 shadow-md shadow-stone-400 border-2 border-stone-200 bg-white rounded-lg mx-auto w-full md:w-[75%]"
-//               >
-//                 <TodoItem todo={todo} />
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default TodoList;
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
 import TodoItem from "./TodoItem";
@@ -57,7 +8,6 @@ const GET_TODOS = gql`
     todos {
       id
       description
-      completed
     }
   }
 `;
@@ -69,7 +19,8 @@ export interface Todo {
 }
 
 const TodoList: React.FC = () => {
-  const { loading, error, data, refetch } = useQuery(GET_TODOS);
+  const { loading, error, data } = useQuery(GET_TODOS);
+  console.log(data);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :</p>;
@@ -85,7 +36,7 @@ const TodoList: React.FC = () => {
                 key={todo.id}
                 className="flex items-center justify-between mb-6 shadow-md shadow-stone-400 border-2 border-stone-200 bg-white rounded-lg mx-auto w-full md:w-[75%]"
               >
-                <TodoItem todo={todo} refetch={refetch} />
+                <TodoItem todo={todo} />
               </div>
             ))}
           </div>
