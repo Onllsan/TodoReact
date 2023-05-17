e060e2958d2c0858d6ff84354a7049a6f07196fc
Onllsan <welcome3me@gmail.com>
2023-05-17 18:54:59 +0200

    y

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,78 +1,36 @@
-// import React, { useState } from "react";
-// import { Todo } from "./TodoList";
+import React, { useState } from "react";
+import { Todo } from "./TodoList";
 
-// interface TodoItemProps {
-//   todo: Todo;
-// }
+interface TodoItemProps {
+  todo: Todo;
+}
 
-// const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
-//   const [completed, setCompleted] = useState(todo.completed);
+const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
+  const [completed, setCompleted] = useState(todo.completed);
 
-//   const handleClick = () => {
-//     setCompleted(!completed);
-//   };
+  const handleClick = () => {
+    setCompleted(!completed);
+  };
 
-//   return (
-//     <div
-//       className="flex items-center p-4 gap-4  "
-//       style={{ cursor: "pointer" }}
-//     >
-//       <button
-//         className={`h-6 w-6 rounded-full border-2 flex-shrink-0 ${
-//           completed ? "border-[#D1A1B5] bg-[#E8B7CB]" : "border-gray-300"
-//         }`}
-//         onClick={handleClick}
-//       />
-//       <div
-//         className={`ml-4 text-md ${completed ? "opacity-50" : ""}`}
-//         onClick={handleClick}
-//       >
-//         {todo.description}
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div
+      className="flex items-center p-4 gap-4  "
+      style={{ cursor: "pointer" }}
+    >
+      <button
+        className={`h-6 w-6 rounded-full border-2 flex-shrink-0 ${
+          completed ? "border-[#D1A1B5] bg-[#E8B7CB]" : "border-gray-300"
+        }`}
+        onClick={handleClick}
+      />
+      <div
+        className={`ml-4 text-md ${completed ? "opacity-50" : ""}`}
+        onClick={handleClick}
+      >
+        {todo.description}
+      </div>
+    </div>
+  );
+};
 
-// export default TodoItem;
-// import React, { useState, useEffect } from "react";
-// import { Todo } from "./TodoList";
-
-// interface TodoItemProps {
-//   todo: Todo;
-// }
-
-// const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
-//   const initialCompletedState = () => {
-//     const savedState = localStorage.getItem(`todo-${todo.id}`);
-//     return savedState ? JSON.parse(savedState) : false;
-//   };
-
-//   const [completed, setCompleted] = useState(initialCompletedState);
-
-//   useEffect(() => {
-//     localStorage.setItem(`todo-${todo.id}`, JSON.stringify(completed));
-//   }, [completed, todo.id]);
-
-//   const handleClick = () => {
-//     setCompleted(!completed);
-//   };
-
-//   return (
-//     <div className="flex items-center p-4 gap-4" style={{ cursor: "pointer" }}>
-//       <button
-//         className={`h-6 w-6 rounded-full border-2 flex-shrink-0 ${
-//           completed ? "border-[#D1A1B5] bg-[#E8B7CB]" : "border-gray-300"
-//         }`}
-//         onClick={handleClick}
-//       />
-//       <div
-//         className={`ml-4 text-md ${completed ? "opacity-50" : ""}`}
-//         onClick={handleClick}
-//       >
-//         {todo.description}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TodoItem;
+export default TodoItem;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,104 +1,49 @@
-// import React from "react";
-// import { useQuery, gql } from "@apollo/client";
-// import TodoItem from "./TodoItem";
-// import Heading from "./Heading";
-
-// const GET_TODOS = gql`
-//   query GetTodos {
-//     todos {
-//       id
-//       description
-//     }
-//   }
-// `;
-
-// export interface Todo {
-//   id: string;
-//   description: string;
-//   completed: boolean;
-// }
-
-// const TodoList: React.FC = () => {
-//   const { loading, error, data } = useQuery(GET_TODOS);
-//   console.log(data);
-
-//   if (loading) return <p>Loading...</p>;
-//   if (error) return <p>Error :</p>;
-
-//   return (
-//     <>
-//       <Heading />
-//       <div className="flex items-center justify-center flex-col m-9 ">
-//         <div className="container">
-//           <div>
-//             {data.todos.map((todo: Todo) => (
-//               <div
-//                 key={todo.id}
-//                 className="flex items-center justify-between mb-6 shadow-md shadow-stone-400 border-2 border-stone-200 bg-white rounded-lg mx-auto w-full md:w-[75%]"
-//               >
-//                 <TodoItem todo={todo} />
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default TodoList;
-
-import { useQuery, useMutation, gql } from "@apollo/client";
-import { useEffect } from "react";
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import TodoItem from "./TodoItem";
+import Heading from "./Heading";
 
 const GET_TODOS = gql`
   query GetTodos {
     todos {
       id
-      title
-      completed
+      description
     }
   }
 `;
 
-const UPDATE_TODO = gql`
-  mutation UpdateTodo($id: ID!, $completed: Boolean!) {
-    updateTodo(id: $id, completed: $completed) {
-      id
-      completed
-    }
-  }
-`;
+export interface Todo {
+  id: string;
+  description: string;
+  completed: boolean;
+}
 
-function TodoList() {
+const TodoList: React.FC = () => {
   const { loading, error, data } = useQuery(GET_TODOS);
-  const [updateTodo] = useMutation(UPDATE_TODO);
+  console.log(data);
 
-  useEffect(() => {
-    if (data) {
-      localStorage.setItem("todos", JSON.stringify(data.todos));
-    }
-  }, [data]);
-
-  if (loading) return "Loading...";
-  if (error) return `Error! ${error.message}`;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
 
   return (
-    <ul>
-      {data.todos.map((id: string, title: string, completed: boolean) => (
-        <li key={id}>
-          <input
-            type="checkbox"
-            checked={completed}
-            onChange={() =>
-              updateTodo({ variables: { id, completed: !completed } })
-            }
-          />
-          {title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <Heading />
+      <div className="flex items-center justify-center flex-col m-9 ">
+        <div className="container">
+          <div>
+            {data.todos.map((todo: Todo) => (
+              <div
+                key={todo.id}
+                className="flex items-center justify-between mb-6 shadow-md shadow-stone-400 border-2 border-stone-200 bg-white rounded-lg mx-auto w-full md:w-[75%]"
+              >
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default TodoList;
