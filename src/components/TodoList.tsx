947476574dff9a5b30decb2dfcb3a59bf2cd42c6
Onllsan <welcome3me@gmail.com>
2023-05-17 16:06:23 +0200

    go back

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,56 +1,3 @@
-// import React from "react";
-// import { useQuery, gql } from "@apollo/client";
-// import TodoItem from "./TodoItem";
-// import Heading from "./Heading";
-
-// const GET_TODOS = gql`
-//   query GetTodos {
-//     todos {
-//       id
-//       description
-//       completed
-//     }
-//   }
-// `;
-
-// export interface Todo {
-//   id: string;
-//   description: string;
-//   completed: boolean;
-// }
-
-// const TodoList: React.FC = () => {
-//   const { loading, error, data } = useQuery(GET_TODOS);
-//   console.log(data);
-
-//   if (loading) return <p>Loading...</p>;
-//   if (error) return <p>Error :</p>;
-
-//   return (
-//     <>
-//       <Heading />
-//       <div className="flex items-center justify-center flex-col m-9 ">
-//         <div className="container">
-//           <div>
-//             {data.todos.map((todo: Todo) => (
-//               <div
-//                 key={todo.id}
-//                 className={`flex items-center justify-between mb-6 shadow-md shadow-stone-400 border-2 border-stone-200 bg-white rounded-lg mx-auto w-full md:w-[75%] ${
-//                   todo.completed ? "bg-black" : ""
-//                 }`}
-
-//               >
-//                 <TodoItem todo={todo} />
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default TodoList;
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
 import TodoItem from "./TodoItem";
@@ -61,7 +8,6 @@ const GET_TODOS = gql`
     todos {
       id
       description
-      completed
     }
   }
 `;
@@ -88,9 +34,7 @@ const TodoList: React.FC = () => {
             {data.todos.map((todo: Todo) => (
               <div
                 key={todo.id}
-                className={`flex items-center justify-between mb-6 shadow-md shadow-stone-400 border-2 border-stone-200 bg-white rounded-lg mx-auto w-full md:w-[75%] ${
-                  todo.completed ? "bg-black" : ""
-                }`}
+                className="flex items-center justify-between mb-6 shadow-md shadow-stone-400 border-2 border-stone-200 bg-white rounded-lg mx-auto w-full md:w-[75%]"
               >
                 <TodoItem todo={todo} />
               </div>
