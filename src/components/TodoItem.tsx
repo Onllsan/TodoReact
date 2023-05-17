4abad040e4e5cb92d4c3e9cb299a6841b849fd41
Onllsan <welcome3me@gmail.com>
2023-05-17 16:23:07 +0200

    completed status

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import { Todo } from "./TodoList";
+import { useMutation, gql } from "@apollo/client";
 
 interface TodoItemProps {
   todo: Todo;
 }
 
+const UPDATE_TODO = gql`
+  mutation UpdateTodo($id: ID!, $completed: Boolean!) {
+    updateTodo(id: $id, completed: $completed) {
+      id
+      description
+      completed
+    }
+  }
+`;
+
 const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
-  const [completed, setCompleted] = useState(todo.completed);
+  const [updateTodo] = useMutation(UPDATE_TODO);
 
   const handleClick = () => {
-    setCompleted(!completed);
+    updateTodo({ variables: { id: todo.id, completed: !todo.completed } });
   };
 
   return (
@@ -19,12 +30,12 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
     >
       <button
         className={`h-6 w-6 rounded-full border-2 flex-shrink-0 ${
-          completed ? "border-[#D1A1B5] bg-[#E8B7CB]" : "border-gray-300"
+          todo.completed ? "border-[#D1A1B5] bg-[#E8B7CB]" : "border-gray-300"
         }`}
         onClick={handleClick}
       />
       <div
-        className={`ml-4 text-md ${completed ? "opacity-50" : ""}`}
+        className={`ml-4 text-md ${todo.completed ? "opacity-50" : ""}`}
         onClick={handleClick}
       >
         {todo.description}
